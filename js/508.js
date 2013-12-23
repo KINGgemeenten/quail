6773bd4f47de21ce92fd3f773bbcdde396161ae0
Kevin Miller <kevin@csumb.edu>
2013-12-22 17:36:48 -0800

    Fixed JS for 508 page.

--- js/508.js
+++ js/508.js
@@ -19,10 +19,15 @@
 						$tr = $('<tr>');
 						$tr.append('<td><strong>' + id + '</strong></td><td>' + sc.title + '</td>');
 						$ul = $('<ul>');
-						$.each(testAlignment[id], function(testName, test) {
-							$ul.append('<li><a href="http://quail.readthedocs.org/en/latest/tests/' + testName +'.html">' + testName + '</a></li>');
-						});
-						$tr.append('<td>' + $ul.html() + '</td>');
+						if(typeof testAlignment[id] !== 'undefined') {
+							$.each(testAlignment[id], function(testName, test) {
+								$ul.append('<li><a href="http://quail.readthedocs.org/en/latest/tests/' + testName +'.html">' + testName + '</a></li>');
+							});
+							$tr.append('<td>' + $ul.html() + '</td>');
+						}
+						else {
+							$tr.append('<td><strong>No tests</strong></td>');
+						}
 						$('#guideline tbody').append($tr);
 					});
 					$('#loading').remove();
